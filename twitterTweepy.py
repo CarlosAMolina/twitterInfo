700b862a09e1227ac73e603b7197cebdc3b5a53c
CarlosAMolina <carlosantmolina@gmail.com>
2018-03-06 05:41:40 +0100

    handle errors

--- twitterTweepy.py
+++ twitterTweepy.py
@@ -3,17 +3,21 @@
 
 # This script works with tweepy
 
-# authentication tutorial:
+# - tweepy installation/authentication tutorial:
 # https://www.digitalocean.com/community/tutorials/how-to-authenticate-a-python-application-with-twitter-using-tweepy-on-ubuntu-14-04
-# tweepy tutorial:
+# - tweepy tutorial:
 # http://docs.tweepy.org/en/v3.5.0/getting_started.html#api
-# twitter and tweepy api reference:
+# - twitter and tweepy api reference:
 # https://developer.twitter.com/en/docs/api-reference-index
 # http://docs.tweepy.org/en/v3.5.0/api.html
-# number with commas as thousands separators:
+# - number with commas as thousands separators:
 # https://stackoverflow.com/questions/1823058/how-to-print-number-with-commas-as-thousands-separators
 
-import tweepy
+try:
+    import tweepy
+    tweepyInstalled = 1
+except:
+    tweepyInstalled = 0
 
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
@@ -26,11 +30,22 @@ TWEETS = [
 'https://twitter.com/DisneySpain/status/915517354477592576'
 ]
 
-AUTH = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-AUTH.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(AUTH)
-
 # functions
+
+def checksAndAlerts():
+    if tweepyInstalled == 0:
+        print 'Error. Install tweepy'
+        return 0
+    if CONSUMER_KEY == '' or CONSUMER_SECRET == '' or ACCESS_TOKEN == '' or ACCESS_TOKEN_SECRET == '':
+        print 'Error. Not Twitter app credentials'
+        return 0
+    return 1
+
+def getApi():
+    AUTH = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    AUTH.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(AUTH)
+    return api
 
 def getTweetValues(TWEET):
     TWEETID = int(TWEET.split('/')[-1])
@@ -53,10 +68,12 @@ def getTweetInfo(TWEETID):
 
 # main
 
-for TWEET in TWEETS:
-    TWEETID, HANDLE = getTweetValues(TWEET)
-    USER, FOLLOWERS = getUserInfo(HANDLE)
-    RETWEETS, LIKES = getTweetInfo(TWEETID)
-    print 'Usuario: ' + USER + '. Handle: @' + HANDLE + '. Seguidores: ' + FOLLOWERS + u'. Retweets publicación: ' + str(RETWEETS) + u'. Likes publicación: ' + str(LIKES) + u'. Dirección publicación:'
-    print TWEET
-    print ''
+if checksAndAlerts() == 1:
+    api = getApi()
+    for TWEET in TWEETS:
+        TWEETID, HANDLE = getTweetValues(TWEET)
+        USER, FOLLOWERS = getUserInfo(HANDLE)
+        RETWEETS, LIKES = getTweetInfo(TWEETID)
+        print 'Usuario: ' + USER + '. Handle: @' + HANDLE + '. Seguidores: ' + FOLLOWERS + u'. Retweets publicación: ' + str(RETWEETS) + u'. Likes publicación: ' + str(LIKES) + u'. Dirección publicación:'
+        print TWEET
+        print ''
